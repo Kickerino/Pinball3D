2f69ec2b47feb99de62e3b39fb084f158a174405
Kickerino H <k.hulst@outlook.com>
2015-10-06 12:21:49 +0200

    Score (WIP)
    An update to the score (Very Work In Progress!!)

---
+++ Assets/scripts/score.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+    private int _score;
+    public Text text;
+    // Use this for initialization
+
+    void Update()
+    {
+        modUi();
+    }
+
+    public void addScore(int i) {
+        _score += i;
+    }
+
+    public void modUi()
+    {
+        string uiText = _score.ToString().PadLeft(9, '0');
+        text.text = uiText;
+    }
+}
+
