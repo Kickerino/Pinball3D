df7d9cf8c97d359f1460ea9c9ebd93085791aac1
Kickerino H <k.hulst@outlook.com>
2015-10-13 11:05:23 +0200

    Score
    Done! Finally! Includes Script & Scene which displays the score (GUI)

---
+++ Assets/scripts/score.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    private int _score = 42069360;
+    public Text text;
+    // Use this for initialization
+
+    void Update()
+    {
+        modUi(); //maybe to be used later on in the project
+    }
+
+    public void addScore(int i)
+    {
+        _score += i;
+    }
+
+    public void modUi()
+    {
+        text.text = utils.neatNumber(_score);
+    }
+}
